290321b071efdd826ab9448c015ca1fc3b7176f2
Kirill Konanykhin <k.konanykhin@gmail.com>
2016-09-12 11:12:44 +0300

    Ft/shmw 3/project skeleton (#1)
    * SHMW-3: Project skeleton
    
    * SHMW-3: Formatting

---
+++ src/main/java/com/rbkmoney/shumway/ShumwayApplication.java
@@ -0,0 +1,12 @@
+package com.rbkmoney.shumway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShumwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShumwayApplication.class, args);
+    }
+}
