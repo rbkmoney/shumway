53f5a15b89246c80cf6aa5d9c4ecc5c7dc935da3
Vladimir Pankrashkin <v.pankrashkin@rbkmoney.com>
2018-05-16 16:46:25 +0300

    SHMW-30: Add CreationTime support

--- src/main/java/com/rbkmoney/shumway/handler/ProtocolConverter.java
+++ src/main/java/com/rbkmoney/shumway/handler/ProtocolConverter.java
@@ -4,6 +4,7 @@ import com.rbkmoney.damsel.accounter.AccountPrototype;
 import com.rbkmoney.damsel.accounter.Posting;
 import com.rbkmoney.damsel.accounter.PostingBatch;
 import com.rbkmoney.damsel.accounter.PostingPlan;
+import com.rbkmoney.geck.common.util.TypeUtil;
 import com.rbkmoney.shumway.domain.*;
 
 import java.time.Instant;
@@ -16,22 +17,51 @@ import java.util.stream.Collectors;
 public class ProtocolConverter {
 
     public static Account convertToDomainAccount(AccountPrototype protocolPrototype) {
-        return new Account(0, Instant.now(), protocolPrototype.getCurrencySymCode(), protocolPrototype.getDescription());
+        return new Account(
+                0,
+                (protocolPrototype.isSetCreationTime() ? TypeUtil.stringToInstant(protocolPrototype.getCreationTime()) : Instant.now()),
+                protocolPrototype.getCurrencySymCode(),
+                protocolPrototype.getDescription()
+        );
     }
 
     public static com.rbkmoney.damsel.accounter.Account convertFromDomainAccount(StatefulAccount domainAccount) {
         AccountState accountState = domainAccount.getAccountState();
-        com.rbkmoney.damsel.accounter.Account protocolAccount = new com.rbkmoney.damsel.accounter.Account(domainAccount.getId(), accountState.getOwnAmount(), accountState.getMaxAvailableAmount(), accountState.getMinAvailableAmount(), domainAccount.getCurrSymCode());
+        com.rbkmoney.damsel.accounter.Account protocolAccount = new com.rbkmoney.damsel.accounter.Account(
+                domainAccount.getId(),
+                accountState.getOwnAmount(),
+                accountState.getMaxAvailableAmount(),
+                accountState.getMinAvailableAmount(),
+                domainAccount.getCurrSymCode()
+        );
+        protocolAccount.setCreationTime(TypeUtil.temporalToString(domainAccount.getCreationTime()));
         protocolAccount.setDescription(domainAccount.getDescription());
         return protocolAccount;
     }
 
     public static Posting convertFromDomainToPosting(PostingLog domainPostingLog) {
-        return new Posting(domainPostingLog.getFromAccountId(), domainPostingLog.getToAccountId(), domainPostingLog.getAmount(), domainPostingLog.getCurrSymCode(), domainPostingLog.getDescription());
+        return new Posting(
+                domainPostingLog.getFromAccountId(),
+                domainPostingLog.getToAccountId(),
+                domainPostingLog.getAmount(),
+                domainPostingLog.getCurrSymCode(),
+                domainPostingLog.getDescription()
+        );
     }
 
-    public static PostingLog convertToDomainPosting(Posting protocolPosting, PostingBatch batch,  com.rbkmoney.shumway.domain.PostingPlanLog currentDomainPlanLog) {
-        return new PostingLog(0, currentDomainPlanLog.getPlanId(), batch.getId(), protocolPosting.getFromId(), protocolPosting.getToId(), protocolPosting.getAmount(), Instant.now(), currentDomainPlanLog.getLastOperation(), protocolPosting.getCurrencySymCode(), protocolPosting.getDescription());
+    public static PostingLog convertToDomainPosting(Posting protocolPosting, PostingBatch batch, com.rbkmoney.shumway.domain.PostingPlanLog currentDomainPlanLog) {
+        return new PostingLog(
+                0,
+                currentDomainPlanLog.getPlanId(),
+                batch.getId(),
+                protocolPosting.getFromId(),
+                protocolPosting.getToId(),
+                protocolPosting.getAmount(),
+                Instant.now(),
+                currentDomainPlanLog.getLastOperation(),
+                protocolPosting.getCurrencySymCode(),
+                protocolPosting.getDescription()
+        );
     }
 
     public static PostingPlanLog convertToDomainPlan(PostingPlan protocolPostingPlan, PostingOperation domainPostingOperation) {
