8ab8dae452106acfd33c855ca20c0930f4ce2f7c
Alexey <kehitt@users.noreply.github.com>
2021-09-29 13:12:47 +0300

    HG-544: Update shumaich proto (#48)
    * update shumaich proto
    
    * Update src/main/java/com/rbkmoney/shumway/handler/ShumaichServiceHandler.java
    
    Co-authored-by: Pospolita Nikita <nikita7asics@gmail.com>
    
    * bump testcontainers?
    
    * Revert "bump testcontainers?"
    
    This reverts commit bdf7dec78c849124070f721020de70b07ae85e8f.
    
    * bump testcontainers
    
    * skip dtrack for epic
    
    Co-authored-by: Pospolita Nikita <nikita7asics@gmail.com>

--- src/main/java/com/rbkmoney/shumway/handler/ShumaichServiceHandler.java
+++ src/main/java/com/rbkmoney/shumway/handler/ShumaichServiceHandler.java
@@ -18,11 +18,16 @@ public class ShumaichServiceHandler implements AccounterSrv.Iface {
 
     @Override
     public Clock hold(PostingPlanChange postingPlanChange, Clock clock) throws TException {
-        log.info("Shumaich in shumway hold method called with postingPlanChange: {}", postingPlanChange);
-        Clock holdClock = shumaichClient.hold(postingPlanChange, clock);
-        accountService.createAccountsIfDontExist(postingPlanChange);
-        shumpuneServiceHandler.hold(ShumaichProtocolConverter.convertToOldPostingPlanChange(postingPlanChange));
-        return holdClock;
+        try {
+            log.info("Shumaich in shumway hold method called with postingPlanChange: {}", postingPlanChange);
+            Clock holdClock = shumaichClient.hold(postingPlanChange, clock);
+            accountService.createAccountsIfDontExist(postingPlanChange);
+            shumpuneServiceHandler.hold(ShumaichProtocolConverter.convertToOldPostingPlanChange(postingPlanChange));
+            return holdClock;
+        } catch (NotReady ex) {
+            log.error("Shumpune is not ready yet", ex);
+            throw ex;
+        }
     }
 
     @Override
